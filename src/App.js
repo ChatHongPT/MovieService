b13c9e242468a3b4ff2c355725c2a34dd356f79a
suk924600 <suk924600@naver.com>
2024-11-24 19:54:51 +0900

    reset

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import MainPage from './pages/MainPage';
+import React, { useState } from 'react';
+import { Outlet, Route, Routes } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <div>
+      <Nav />
+
+      <Outlet />
+
+      <Footer />
+    </div>
+  );
+};
+
+export const CommonStateContext = React.createContext();
 
 function App() {
+  const [isClicked, setIsClicked] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CommonStateContext.Provider value={{isClicked, setIsClicked}}>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Layout />} >
+            <Route index element={<MainPage/>} />
+          </Route>  
+        </Routes>  
+      </div>
+    </CommonStateContext.Provider>
   );
 }
 
